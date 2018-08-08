af1441d0be3d513590e91ee963c0cd0d54a91ed2
Takashi Kitajima <inc@2inc.org>
2018-08-09 08:40:53 +0900

    Some fixes

--- snow-monkey-awesome-custom-blocks.php
+++ snow-monkey-awesome-custom-blocks.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin name: Snow Monkey Awesome Custom Blocks
- * Version: 0.1.0
+ * Version: 0.1.1
  * Text Domain: snow-monkey-awesome-custom-blocks
  * Domain Path: /languages/
  *
