e55c9ea6ee3fcc87594b01e57c030b6b41616a65
Takashi Kitajima <inc@2inc.org>
2018-09-12 14:35:22 +0900

    Fix #24

--- block/section-has-bgimage/block.js
+++ block/section-has-bgimage/block.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const { registerBlockType } = wp.blocks;
-const { RichText, InnerBlocks, InspectorControls, MediaUpload } = wp.editor;
-const { Button, PanelBody, SelectControl } = wp.components;
+const { RichText, InnerBlocks, InspectorControls, MediaUpload, ColorPalette } = wp.editor;
+const { Button, PanelBody, SelectControl, RangeControl, BaseControl } = wp.components;
 const { Fragment } = wp.element;
 const { __ } = wp.i18n;
 
@@ -36,13 +36,21 @@ registerBlockType( 'snow-monkey-blocks/section-has-bgimage', {
 			type: 'string',
 			default: 'left',
 		},
+		maskColor: {
+			type: 'string',
+			default: '#000',
+		},
+		maskOpacity: {
+			type: 'number',
+			default: 1,
+		},
 	},
 	supports: {
 		align: [ 'wide', 'full' ],
 	},
 
 	edit( { attributes, setAttributes, isSelected } ) {
-		const { title, imageID, imageURL, height, contentsAlignment } = attributes;
+		const { title, imageID, imageURL, height, contentsAlignment, maskColor, maskOpacity } = attributes;
 
 		const renderImage = ( obj ) => {
 			return (
@@ -91,10 +99,27 @@ registerBlockType( 'snow-monkey-blocks/section-has-bgimage', {
 							] }
 							onChange={ ( value ) => setAttributes( { contentsAlignment: value } ) }
 						/>
+
+						<BaseControl label={ __( 'Mask Color', 'snow-monkey-blocks' ) }>
+							<ColorPalette
+								value={ maskColor }
+								onChange={ ( value ) => setAttributes( { maskColor: value } ) }
+							/>
+						</BaseControl>
+
+						<RangeControl
+							label={ __( 'Mask Opacity', 'snow-monkey-blocks' ) }
+							value={ maskOpacity }
+							onChange={ ( value ) => setAttributes( { maskOpacity: value } ) }
+							min={ 0 }
+							max={ 1 }
+							step={ 0.1 }
+						/>
 					</PanelBody>
 				</InspectorControls>
 				<div className={ `smb-section smb-section-has-bgimage smb-section-has-bgimage--${ contentsAlignment } smb-section-has-bgimage--${ height }` }>
-					<div className="smb-section-has-bgimage__bgimage">
+					<div className="smb-section-has-bgimage__mask" style={ { backgroundColor: maskColor } }></div>
+					<div className="smb-section-has-bgimage__bgimage" style={ { opacity: maskOpacity } }>
 						<MediaUpload
 							onSelect={ ( media ) => {
 								const newImageURL = !! media.sizes.xlarge ? media.sizes.xlarge.url : media.url;
@@ -127,11 +152,12 @@ registerBlockType( 'snow-monkey-blocks/section-has-bgimage', {
 	},
 
 	save( { attributes } ) {
-		const { title, imageURL, height, contentsAlignment } = attributes;
+		const { title, imageURL, height, contentsAlignment, maskColor, maskOpacity } = attributes;
 
 		return (
 			<div className={ `smb-section smb-section-has-bgimage smb-section-has-bgimage--${ contentsAlignment } smb-section-has-bgimage--${ height }` }>
-				<div className="smb-section-has-bgimage__bgimage">
+				<div className="smb-section-has-bgimage__mask" style={ { backgroundColor: maskColor } }></div>
+				<div className="smb-section-has-bgimage__bgimage" style={ { opacity: maskOpacity } }>
 					<img src={ imageURL } alt="" />
 				</div>
 				<div className="c-container">
@@ -147,4 +173,58 @@ registerBlockType( 'snow-monkey-blocks/section-has-bgimage', {
 			</div>
 		);
 	},
+
+	deprecated: [
+		{
+			attributes: {
+				title: {
+					type: 'array',
+					source: 'children',
+					selector: '.smb-section__title',
+					default: [],
+				},
+				imageID: {
+					type: 'number',
+					default: 0,
+				},
+				imageURL: {
+					type: 'string',
+					source: 'attribute',
+					selector: '.smb-section-has-bgimage__bgimage > img',
+					attribute: 'src',
+					default: smb.pluginURL + 'block/section-has-bgimage/image.png',
+				},
+				height: {
+					type: 'string',
+					default: 'fit',
+				},
+				contentsAlignment: {
+					type: 'string',
+					default: 'left',
+				},
+			},
+
+			save( { attributes } ) {
+				const { title, imageURL, height, contentsAlignment } = attributes;
+
+				return (
+					<div className={ `smb-section smb-section-has-bgimage smb-section-has-bgimage--${ contentsAlignment } smb-section-has-bgimage--${ height }` }>
+						<div className="smb-section-has-bgimage__bgimage">
+							<img src={ imageURL } alt="" />
+						</div>
+						<div className="c-container">
+							{ title.length > 0 &&
+								<h2 className="smb-section__title">
+									{ title }
+								</h2>
+							}
+							<div className="smb-section__body">
+								<InnerBlocks.Content />
+							</div>
+						</div>
+					</div>
+				);
+			},
+		},
+	],
 } );
