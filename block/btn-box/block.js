44c1a0b61e9324cbd2d61006b5e070927a3cf1d6
Takashi Kitajima <inc@2inc.org>
2018-10-03 23:24:55 +0900

    Fix #35

--- block/btn-box/block.js
+++ block/btn-box/block.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import classnames from 'classnames';
+
 const { registerBlockType } = wp.blocks;
 const { RichText, InspectorControls, ColorPalette } = wp.editor;
 const { PanelBody, BaseControl, TextControl, SelectControl } = wp.components;
@@ -49,13 +51,17 @@ registerBlockType( 'snow-monkey-blocks/btn-box', {
 			type: 'string',
 			default: null,
 		},
+		btnSize: {
+			type: 'string',
+			default: null,
+		},
 	},
 	supports: {
 		align: [ 'wide', 'full' ],
 	},
 
 	edit( { attributes, setAttributes, isSelected } ) {
-		const { lede, note, backgroundColor, btnLabel, btnURL, btnTarget, btnBackgroundColor, btnTextColor } = attributes;
+		const { lede, note, backgroundColor, btnLabel, btnURL, btnTarget, btnBackgroundColor, btnTextColor, btnSize } = attributes;
 
 		return (
 			<Fragment>
@@ -108,6 +114,22 @@ registerBlockType( 'snow-monkey-blocks/btn-box', {
 								onChange={ ( value ) => setAttributes( { btnTextColor: value } ) }
 							/>
 						</BaseControl>
+
+						<SelectControl
+							label={ __( 'Button size', 'snow-monkey-blocks' ) }
+							value={ btnSize }
+							onChange={ ( value ) => setAttributes( { btnSize: value } ) }
+							options={ [
+								{
+									value: '',
+									label: __( 'Normal button', 'snow-monkey-blocks' ),
+								},
+								{
+									value: 'full',
+									label: __( 'Full button', 'snow-monkey-blocks' ),
+								},
+							] }
+						/>
 					</PanelBody>
 				</InspectorControls>
 
@@ -123,19 +145,21 @@ registerBlockType( 'snow-monkey-blocks/btn-box', {
 							/>
 						}
 
-						<span
-							className="smb-btn smb-btn--full" href={ btnURL } target={ btnTarget }
-							style={ { backgroundColor: btnBackgroundColor } }
-						>
-							<RichText
-								className="smb-btn__label"
-								value={ btnLabel }
-								placeholder={ __( 'Button', 'snow-monkey-blocks' ) }
-								onChange={ ( value ) => setAttributes( { btnLabel: value } ) }
-								style={ { color: btnTextColor } }
-								formattingControls={ [] }
-							/>
-						</span>
+						<div className="smb-btn-box__btn-wrapper">
+							<span
+								className={ classnames( 'smb-btn', { [ `smb-btn--${ btnSize }` ]: !! btnSize } ) } href={ btnURL } target={ btnTarget }
+								style={ { backgroundColor: btnBackgroundColor } }
+							>
+								<RichText
+									className="smb-btn__label"
+									value={ btnLabel }
+									placeholder={ __( 'Button', 'snow-monkey-blocks' ) }
+									onChange={ ( value ) => setAttributes( { btnLabel: value } ) }
+									style={ { color: btnTextColor } }
+									formattingControls={ [] }
+								/>
+							</span>
+						</div>
 
 						{ ( note.length > 0 || isSelected ) &&
 							<RichText
@@ -153,7 +177,7 @@ registerBlockType( 'snow-monkey-blocks/btn-box', {
 	},
 
 	save( { attributes } ) {
-		const { lede, note, backgroundColor, btnLabel, btnURL, btnTarget, btnBackgroundColor, btnTextColor } = attributes;
+		const { lede, note, backgroundColor, btnLabel, btnURL, btnTarget, btnBackgroundColor, btnTextColor, btnSize } = attributes;
 
 		return (
 			<div className="smb-btn-box" style={ { backgroundColor: backgroundColor } }>
@@ -164,14 +188,16 @@ registerBlockType( 'snow-monkey-blocks/btn-box', {
 						</div>
 					}
 
-					<a
-						className="smb-btn smb-btn--full" href={ btnURL } target={ btnTarget }
-						style={ { backgroundColor: btnBackgroundColor } }
-					>
-						<span className="smb-btn__label" style={ { color: btnTextColor } }>
-							{ btnLabel }
-						</span>
-					</a>
+					<div className="smb-btn-box__btn-wrapper">
+						<a
+							className={ classnames( 'smb-btn', { [ `smb-btn--${ btnSize }` ]: !! btnSize } ) } href={ btnURL } target={ btnTarget }
+							style={ { backgroundColor: btnBackgroundColor } }
+						>
+							<span className="smb-btn__label" style={ { color: btnTextColor } }>
+								{ btnLabel }
+							</span>
+						</a>
+					</div>
 
 					{ note.length > 0 &&
 						<div className="smb-btn-box__note">
@@ -182,4 +208,93 @@ registerBlockType( 'snow-monkey-blocks/btn-box', {
 			</div>
 		);
 	},
+
+	deprecated: [
+		{
+			attributes: {
+				lede: {
+					type: 'array',
+					source: 'children',
+					selector: '.smb-btn-box__lede',
+					default: [],
+				},
+				note: {
+					type: 'array',
+					source: 'children',
+					selector: '.smb-btn-box__note',
+					default: [],
+				},
+				backgroundColor: {
+					type: 'string',
+					default: null,
+				},
+				btnLabel: {
+					type: 'array',
+					source: 'children',
+					selector: '.smb-btn__label',
+					default: [ __( 'Button', 'snow-monkey-blocks' ) ],
+				},
+				btnURL: {
+					type: 'string',
+					default: '',
+				},
+				btnTarget: {
+					type: 'string',
+					default: '_self',
+				},
+				btnBackgroundColor: {
+					type: 'string',
+					default: null,
+				},
+				btnTextColor: {
+					type: 'string',
+					default: null,
+				},
+			},
+
+			save( { attributes } ) {
+				const { lede, note, backgroundColor, btnLabel, btnURL, btnTarget, btnBackgroundColor, btnTextColor } = attributes;
+
+				const btnBoxStyle = {};
+				if ( !! backgroundColor && 'null' !== backgroundColor ) {
+					btnBoxStyle.backgroundColor = backgroundColor;
+				}
+
+				const btnStyle = {};
+				if ( !! btnBackgroundColor && 'null' !== btnBackgroundColor ) {
+					btnStyle.btnBackgroundColor = btnBackgroundColor;
+				}
+
+				const btnLabelStyle = {};
+				if ( !! btnTextColor && 'null' !== btnTextColor ) {
+					btnLabelStyle.btnTextColor = btnTextColor;
+				}
+
+				return (
+					<div className="smb-btn-box" style={ btnBoxStyle }>
+						<div className="c-container">
+							{ lede.length > 0 &&
+								<div className="smb-btn-box__lede">
+									{ lede }
+								</div>
+							}
+
+							<a
+								className="smb-btn smb-btn--full" href={ btnURL } target={ btnTarget } style={ btnStyle }>
+								<span className="smb-btn__label" style={ btnLabelStyle }>
+									{ btnLabel }
+								</span>
+							</a>
+
+							{ note.length > 0 &&
+								<div className="smb-btn-box__note">
+									{ note }
+								</div>
+							}
+						</div>
+					</div>
+				);
+			},
+		},
+	],
 } );
