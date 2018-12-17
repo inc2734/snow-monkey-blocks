410d968426aa60dbf2ca5a867f5a9c718dedb9f7
Takashi Kitajima <inc@2inc.org>
2018-12-17 09:40:50 +0900

    #79 block.php のリファクタリング

--- block/categories-list/block.php
+++ block/categories-list/block.php
@@ -1,51 +1,61 @@
 <?php
-	
-	$categories = get_categories();
-	$block_content = '';
-	if ( ! empty( $categories ) ) {
-		$output_markup = '';
-		$articles = esc_html__( 'articles', 'snow-monkey-blocks' );
-		$latest_posts_count = esc_html__( '%1$d latest_posts', 'snow-monkey-blocks' );
-		$no_title = esc_html__( 'no title', 'snow-monkey-blocks' );
+/**
+ * @package snow-monkey-blocks
+ * @author kmix-39
+ * @license GPL-2.0+
+ */
 
-		foreach( $categories as $category ) {
-			$category_detail = get_category( $category );
-			$output = '<li class="smb-categories-list__cat">';
-			$output .= sprintf( '<h4>%1$d<span>%2$s</span></h4>', $category_detail->count, $articles );
-			$output .= '<div class="detail">';
-			$output .= '<h5><a href="' . get_category_link( $category_detail->term_id ) . '">' . $category_detail->cat_name . '</a></h5>';
-			$output .= '<span class="description">' . $category_detail->category_description . '</span>';
-			if ( ! empty( $attributes['articles'] ) ) {
-				$output .= '<h6>' . sprintf( $latest_posts_count, $attributes['articles'] ) . '</h6>';
-				$output .= '<ul class="latest-articles-list">';
-				$posts = get_posts(
-					array(
-						'category' => $category_detail->term_id,
-						'showposts' => (string) $attributes['articles'],
-						'order' => 'ASC'
-					)
-				);
-				global $post;
-				if ( $posts ) {
-					foreach( $posts as $post ) {
-						setup_postdata( $post );
-						$title = get_the_title() !== '' ? get_the_title() : $no_title;
-						$output .= '<li><a href="' . get_permalink() . '">' . $title . '</a></li>';
-					}
-				}
-				$output .= '</ul>';
-			}
-			
-			$output .= '</div>';
-			$output .= '</li>';
-			$output_markup .= $output;
-		}
+$categories = get_categories();
 
-		$wrapper_markup = '<ul class="smb-categories-list">%1$s</ul>';
-		$block_content = sprintf(
-			$wrapper_markup,
-			$output_markup
-		);
-	}
-	echo $block_content;
+ob_start();
+?>
+<ul class="smb-categories-list">
+	<?php foreach ( $categories as $category ) : ?>
+		<?php $category_detail = get_category( $category ); ?>
 
+		<li class="smb-categories-list__cat">
+			<h4>
+				<?php echo esc_html( $category_detail->count ); ?>
+				<span><?php esc_html_e( 'articles', 'snow-monkey-blocks' ); ?></span>
+			</h4>
+
+			<div class="detail">
+				<h5>
+					<a href="<?php echo esc_url( get_category_link( $category_detail->term_id ) ); ?>">
+						<?php echo esc_html( $category_detail->cat_name ); ?>
+					</a>
+				</h5>
+				<span class="description">
+					<?php echo wp_kses_post( $category_detail->category_description ); ?>
+				</span>
+
+				<?php if ( ! empty( $attributes['articles'] ) ) : ?>
+					<h6><?php echo esc_html( sprintf( __( '%1$d latest_posts', 'snow-monkey-blocks' ), $attributes['articles'] ) ); ?></h6>
+					<ul class="latest-articles-list">
+						<?php
+						$_wp_query = new \WP_Query(
+							[
+								'category'            => $category_detail->term_id,
+								'showposts'           => (string) $attributes['articles'],
+								'order'               => 'ASC',
+								'ignore_sticky_posts' => true,
+								'no_found_rows'       => true,
+								'suppress_filters'    => true,
+							]
+						);
+						?>
+						<?php if ( $_wp_query->have_posts() ) : ?>
+							<?php while ( $_wp_query->have_posts() ) : ?>
+								<?php $_wp_query->the_post(); ?>
+								<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+							<?php endwhile; ?>
+							<?php wp_reset_postdata(); ?>
+						<?php endif; ?>
+					</ul>
+				<?php endif; ?>
+			</div>
+		</li>
+	<?php endforeach; ?>
+</ul>
+<?php
+echo wp_kses_post( ob_get_clean() );
