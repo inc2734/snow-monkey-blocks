48dd555a170bedac2a021db4b0c9392abea7236b
Takashi Kitajima <inc@2inc.org>
2019-03-19 10:59:14 +0900

    Some fixes

--- bin/wpphpunit.sh
+++ bin/wpphpunit.sh
@@ -2,21 +2,23 @@
 
 set -e;
 
-WP_TESTS_DIR=${WP_TESTS_DIR-/tmp/wordpress-tests-lib}
-WP_CORE_DIR=${WP_CORE_DIR-/tmp/wordpress/}
+TMPDIR=${TMPDIR-/tmp}
+TMPDIR=$(echo $TMPDIR | sed -e "s/\/$//")
+WP_TESTS_DIR=${WP_TESTS_DIR-$TMPDIR/wordpress-tests-lib}
+WP_CORE_DIR=${WP_CORE_DIR-$TMPDIR/wordpress/}
 
 themedir=$(pwd)
 
 cd ${themedir}
 
-if [ -e ${themedir}/bin/install-wp-tests.sh ]; then
+if [ -e ${themedir}/bin/install-wp-tests.sh ] || [ ! -e ${WP_TESTS_DIR}/includes/functions.php ]; then
   echo 'DROP DATABASE IF EXISTS wordpress_test;' | mysql -u root
 
-  if [ -e ${WP_CORE_DIR} ]; then
+  if [ -d ${WP_CORE_DIR} ]; then
     rm -fr ${WP_CORE_DIR}
   fi
 
-  if [ -e ${WP_TESTS_DIR} ]; then
+  if [ -d ${WP_TESTS_DIR} ]; then
     rm -fr ${WP_TESTS_DIR}
   fi
 
