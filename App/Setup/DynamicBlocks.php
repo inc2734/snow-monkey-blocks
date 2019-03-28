20cc9ffa54b96e171585997d6564a7ca978b08f4
kmix-39 <cheero.energy@gmail.com>
2019-03-28 22:52:52 +0900

    DynamicBlocksでコンテンツを返却できるように対応

--- App/Setup/DynamicBlocks.php
+++ App/Setup/DynamicBlocks.php
@@ -23,7 +23,7 @@ class DynamicBlocks {
 	 * @param array $attributes This variable can be referenced in the template.
 	 * @return string
 	 */
-	public static function render( $slug, $attributes ) {
+	public static function render( $slug, $attributes, $content = null ) {
 		ob_start();
 		include( SNOW_MONKEY_BLOCKS_DIR_PATH . '/block/' . $slug . '/view.php' );
 		return ob_get_clean();
