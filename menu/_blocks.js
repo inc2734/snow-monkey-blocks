475d8209acfcb9d25f85cb975f9990664ca66352
kmix-39 <cheero.energy@gmail.com>
2019-06-13 09:48:01 +0900

    画像ロードとアラートブロックを適当なInsertする程度の仕組み化

--- menu/_blocks.js
+++ menu/_blocks.js
@@ -1,25 +1,40 @@
 'use strict';
 
+import Img from 'react-image';
+
 import { blockConfig } from '../src/js/config/block.js';
 
 const { Component } = wp.element;
-const { PanelBody } = wp.components;
+const { compose } = wp.compose;
+const { PanelBody, Modal, Button, Spinner } = wp.components;
 const { Fragment } = wp.element;
 const { __ } = wp.i18n;
+const { parse } = wp.blocks;
 
-export class MenuBlocks extends Component {
+class MenuBlocks extends Component {
 	constructor() {
 		super( ...arguments );
+
+		this.state = {
+			loading: false,
+			data: [],
+			selectBlock: null,
+			selectScreenShot: 'https://snow-monkey.2inc.org/wp-content/uploads/2018/10/screenshot-1-1920x1440.png',
+		};
+		// カテゴリーのロード
 		this.getBlocksFromCategory = this.getBlocksFromCategory.bind( this );
 	}
 
 	render() {
+		const {
+			insertBlocks,
+		} = wp.data.dispatch( 'core/editor' );
+
 		const categories = wp.blocks.getCategories();
 		const smbCategories = blockConfig.blockCategories;
 		const resultCategories = [];
 		categories.map( ( category ) => {
 			Object.keys( smbCategories ).map( ( name ) => {
-				console.log( smbCategories[ name ] );
 				if ( category.slug === smbCategories[ name ] ) {
 					const resultBlocks = this.getBlocksFromCategory( category.slug );
 					resultCategories.push(
@@ -38,8 +53,46 @@ export class MenuBlocks extends Component {
 		return (
 			<Fragment>
 				{ resultCategories }
+				{ this.state.selectBlock !== null && (
+					<Modal
+						title={ __( 'Block Image', 'snow-monkey-blocks' ) }
+						onRequestClose={ () => this.setDetailState( null ) }
+					>
+						<Img
+							src={ this.state.selectScreenShot }
+							loader={
+								<Spinner />
+							}
+						/>
+						<Button
+							isDefault
+							onClick={ () => {
+								const parsedBlocks = parse( '<!-- wp:snow-monkey-blocks/balloon {"balloonName":"Test"} --><div class="wp-block-snow-monkey-blocks-balloon smb-balloon"><div class="smb-balloon__person"><div class="smb-balloon__figure"><img src="https://0.gravatar.com/avatar/00000000000000000000000000000000?s=128&amp;d=mp&amp;r=g" alt="" class="wp-image-0"/></div><div class="smb-balloon__name">Test</div></div><div class="smb-balloon__body"></div></div><!-- /wp:snow-monkey-blocks/balloon -->' );
+								if ( parsedBlocks.length ) {
+									insertBlocks( parsedBlocks );
+								}
+								this.setDetailState( null )
+							} }
+						>
+							{ __( 'Insert Block', 'snow-monkey-blocks' ) }
+						</Button>
+					</Modal>
+				) }
 			</Fragment>
 		);
+	}
+
+	setDetailState( selectBlock ) {
+		this.setState( { selectBlock: selectBlock } );
+	}
+
+	setBlockScreenShot() {
+		if ( this.state.selectBlock !== null ) {
+			// 読み込んだデータの中にある場合、そのURLを返却
+			this.setState( { selectScreenShot: selectScreenShot } );
+			return;
+		}
+		this.setState( { selectScreenShot: null } );
 	}
 
 	getBlocksFromCategory( category ) {
@@ -59,13 +112,19 @@ export class MenuBlocks extends Component {
 				}
 				result.push(
 					<li>
-						<p>{ icon }{ block.title }</p>
-						<p>{ block.description }</p>
+						<Button
+							isDefault
+							onClick={ () => this.setDetailState( block.name ) }
+						>
+							<p>{ icon }{ block.title }</p>
+							<p>{ block.description }</p>
+						</Button>
 					</li>
 				);
 			}
 		} );
 		return result;
 	}
-
 }
+
+export { MenuBlocks };
