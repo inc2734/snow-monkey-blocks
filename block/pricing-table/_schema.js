eeaf6c8c1c8ef7a886a4a61dd2b4dee08613d4a0
Takashi Kitajima <inc@2inc.org>
2019-07-09 00:13:37 +0900

    Fix #177

---
+++ block/pricing-table/_schema.js
@@ -0,0 +1,7 @@
+'use strict';
+
+export const schema = {
+	columnSize: {
+		type: 'string',
+	},
+};
--- block/pricing-table/block.js
+++ block/pricing-table/block.js
@@ -3,10 +3,13 @@
 import classnames from 'classnames';
 
 import { blockConfig } from '../../src/js/config/block.js';
+import { schema } from './_schema.js';
 import { deprecated } from './_deprecated.js';
 
 const { registerBlockType } = wp.blocks;
-const { InnerBlocks } = wp.editor;
+const { InnerBlocks, InspectorControls } = wp.editor;
+const { PanelBody, BaseControl, SelectControl } = wp.components;
+const { Fragment } = wp.element;
 const { __ } = wp.i18n;
 
 registerBlockType( 'snow-monkey-blocks/pricing-table', {
@@ -17,31 +20,85 @@ registerBlockType( 'snow-monkey-blocks/pricing-table', {
 		src: 'warning',
 	},
 	category: blockConfig.blockCategories.common,
+	attributes: schema,
 	snowMonkeyBlocks: {
 		screenshot: `${ smb.pluginUrl }/dist/img/screenshot/block/pricing-table.png`,
 	},
 
-	edit( { className } ) {
+	edit( { attributes, setAttributes, className } ) {
+		const { columnSize } = attributes;
+
+		const classes = classnames(
+			{
+				'smb-pricing-table': true,
+				[ `smb-pricing-table--col-size-${ columnSize }` ]: !! columnSize,
+				[ className ]: true,
+			}
+		);
+
 		const allowedBlocks = [ 'snow-monkey-blocks/pricing-table--item' ];
 		const template = [ [ 'snow-monkey-blocks/pricing-table--item' ] ];
 
-		const classes = classnames( 'smb-pricing-table', className );
-
 		return (
-			<div className={ classes }>
-				<div className="c-row c-row--md-nowrap" data-columns>
-					<InnerBlocks
-						allowedBlocks={ allowedBlocks }
-						template={ template }
-						templateLock={ false }
-					/>
+			<Fragment>
+				<InspectorControls>
+					<PanelBody title={ __( 'Block Settings', 'snow-monkey-blocks' ) }>
+						<BaseControl
+							label={ __( 'Column Size', 'snow-monkey-blocks' ) }
+							help={ __( 'If the text of each item is long, it is recommended to select other than "Auto".', 'snow-monkey-blocks' ) }>
+							<SelectControl
+								value={ columnSize }
+								options={ [
+									{
+										value: '',
+										label: __( 'Auto', 'snow-monkey-blocks' ),
+									},
+									{
+										value: '1-4',
+										label: __( '25%', 'snow-monkey-blocks' ),
+									},
+									{
+										value: '1-3',
+										label: __( '33%', 'snow-monkey-blocks' ),
+									},
+									{
+										value: '1-2',
+										label: __( '50%', 'snow-monkey-blocks' ),
+									},
+									{
+										value: '1-1',
+										label: __( '100%', 'snow-monkey-blocks' ),
+									},
+								] }
+								onChange={ ( value ) => setAttributes( { columnSize: value } ) }
+							/>
+						</BaseControl>
+					</PanelBody>
+				</InspectorControls>
+
+				<div className={ classes }>
+					<div className="c-row c-row--md-nowrap">
+						<InnerBlocks
+							allowedBlocks={ allowedBlocks }
+							template={ template }
+							templateLock={ false }
+						/>
+					</div>
 				</div>
-			</div>
+			</Fragment>
 		);
 	},
 
-	save( { className } ) {
-		const classes = classnames( 'smb-pricing-table', className );
+	save( { attributes, className } ) {
+		const { columnSize } = attributes;
+
+		const classes = classnames(
+			{
+				'smb-pricing-table': true,
+				[ `smb-pricing-table--col-size-${ columnSize }` ]: !! columnSize,
+				[ className ]: true,
+			}
+		);
 
 		return (
 			<div className={ classes }>
