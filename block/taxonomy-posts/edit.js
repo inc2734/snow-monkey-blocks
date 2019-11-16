6e6d4ef602d3efd384e239efd09023eb32414cd6
Takashi Kitajima <inc@2inc.org>
2019-11-16 15:00:18 +0900

    #224 refactoring taxonomy-posts

--- block/taxonomy-posts/edit.js
+++ block/taxonomy-posts/edit.js
@@ -1,29 +1,152 @@
 'use strict';
 
-import TaxonomyPosts from './taxonomy-posts';
+import { toNumber, buildTermsTree } from '../../src/js/helper/helper';
 
 import {
-	withSelect,
+	find,
+} from 'lodash';
+
+import {
+	InspectorControls,
+	InspectorAdvancedControls,
+} from '@wordpress/block-editor';
+
+import {
+	PanelBody,
+	SelectControl,
+	RangeControl,
+	ToggleControl,
+	TextControl,
+	TreeSelect,
+	Placeholder,
+	Spinner,
+} from '@wordpress/components';
+
+import {
+	useSelect,
 } from '@wordpress/data';
 
-export default withSelect( ( select ) => {
-	const { getTaxonomies, getEntityRecords } = select( 'core' );
+import {
+	Fragment,
+} from '@wordpress/element';
 
-	const AllTaxonomies = getTaxonomies( { per_page: -1 } ) || [];
-	const taxonomies = AllTaxonomies.filter( ( taxonomy ) => taxonomy.visibility.show_ui );
+import {
+	__,
+} from '@wordpress/i18n';
 
-	const taxonomiesTerms = {};
-	taxonomies.forEach( ( taxonomy ) => {
-		const terms = getEntityRecords( 'taxonomy', taxonomy.slug, { per_page: -1 } ) || [];
-		if ( 0 < terms.length ) {
-			taxonomiesTerms[ taxonomy.slug ] = terms;
-		}
-	} );
+import ServerSideRender from '@wordpress/server-side-render';
 
-	return {
-		withSelect: {
-			taxonomies: taxonomies,
-			taxonomiesTerms: taxonomiesTerms,
+export default function( { attributes, setAttributes } ) {
+	const { taxonomy, termId, postsPerPage, layout, ignoreStickyPosts, myAnchor } = attributes;
+
+	const { taxonomiesTerms, taxonomies } = useSelect(
+		( select ) => {
+			const { getTaxonomies, getEntityRecords } = select( 'core' );
+			const allTaxonomies = getTaxonomies( { per_page: -1 } ) || [];
+			const shownTaxonomies = allTaxonomies.filter( ( _taxonomy ) => _taxonomy.visibility.show_ui );
+
+			const _taxonomiesTerms = shownTaxonomies.map( ( _taxonomy ) => {
+				const terms = getEntityRecords( 'taxonomy', _taxonomy.slug, { per_page: -1 } ) || [];
+				if ( 0 < terms.length ) {
+					return {
+						taxonomy: _taxonomy.slug,
+						terms: terms,
+					};
+				}
+			} ).filter( ( taxonomyTerms ) => taxonomyTerms );
+
+			return {
+				taxonomiesTerms: _taxonomiesTerms,
+				taxonomies: shownTaxonomies,
+			};
 		},
+		[ attributes ]
+	);
+
+	const terms = find( taxonomiesTerms, { taxonomy } );
+	const selectedTerm = !! terms ? find( terms.terms, [ 'id', toNumber( termId ) ] ) : [];
+
+	const Loading = () => {
+		return (
+			<Placeholder
+				icon="editor-ul"
+				label={ __( 'Taxonomy posts', 'snow-monkey-blocks' ) }
+			>
+				<Spinner />
+			</Placeholder>
+		);
 	};
-} )( TaxonomyPosts );
+
+	return (
+		<Fragment>
+			<InspectorControls>
+				<PanelBody title={ __( 'Block Settings', 'snow-monkey-blocks' ) }>
+					{ taxonomiesTerms.map( ( taxonomyTerms ) => {
+						const _taxonomy = find( taxonomies, [ 'slug', taxonomyTerms.taxonomy ] );
+						return !! _taxonomy && (
+							<TreeSelect
+								key={ `${ _taxonomy.slug }-${ termId }` }
+								label={ _taxonomy.name }
+								noOptionLabel="-"
+								onChange={ ( value ) => setAttributes( { taxonomy: _taxonomy.slug, termId: toNumber( value ) } ) }
+								selectedId={ termId }
+								tree={ buildTermsTree( taxonomyTerms.terms ) }
+							/>
+						);
+					} ) }
+
+					<RangeControl
+						label={ __( 'Number of posts', 'snow-monkey-blocks' ) }
+						value={ postsPerPage }
+						onChange={ ( value ) => setAttributes( { postsPerPage: toNumber( value, 1, 12 ) } ) }
+						min="1"
+						max="12"
+					/>
+
+					<SelectControl
+						label={ __( 'Layout', 'snow-monkey-blocks' ) }
+						value={ layout }
+						onChange={ ( value ) => setAttributes( { layout: value } ) }
+						options={ [
+							{
+								value: 'rich-media',
+								label: __( 'Rich media', 'snow-monkey-blocks' ),
+							},
+							{
+								value: 'simple',
+								label: __( 'Simple', 'snow-monkey-blocks' ),
+							},
+							{
+								value: 'text',
+								label: __( 'Text', 'snow-monkey-blocks' ),
+							},
+						] }
+					/>
+
+					<ToggleControl
+						label={ __( 'Ignore sticky posts', 'snow-monkey-blocks' ) }
+						checked={ ignoreStickyPosts }
+						onChange={ ( value ) => setAttributes( { ignoreStickyPosts: value } ) }
+					/>
+				</PanelBody>
+			</InspectorControls>
+			<InspectorAdvancedControls>
+				<TextControl
+					label={ __( 'HTML Anchor', 'snow-monkey-blocks' ) }
+					help={ __( 'Anchors lets you link directly to a section on a page.', 'snow-monkey-blocks' ) }
+					value={ myAnchor || '' }
+					onChange={ ( value ) => setAttributes( { myAnchor: value.replace( /[\s#]/g, '-' ) } ) }
+				/>
+			</InspectorAdvancedControls>
+
+			{ ! selectedTerm || ! terms ? (
+				<Loading />
+			) : (
+				<ServerSideRender
+					block="snow-monkey-blocks/taxonomy-posts"
+					attributes={ attributes }
+				/>
+			) }
+		</Fragment>
+	);
+}
