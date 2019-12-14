efe7509627dbcd84501e5979c8587fbd9da7b5d6
Takashi Kitajima <inc@2inc.org>
2019-12-14 16:52:20 +0900

    #240 evaluation-star

--- block/evaluation-star/block/index.js
+++ block/evaluation-star/block/index.js
@@ -5,6 +5,7 @@ import attributes from './attributes';
 import edit from './edit';
 import save from './save';
 import deprecated from './deprecated';
+import example from './example';
 
 import {
 	__,
@@ -20,11 +21,9 @@ export const settings = {
 		src: 'star-half',
 	},
 	category: blockConfig.blockCategories.common,
-	snowMonkeyBlocks: {
-		screenshot: `${ smb.pluginUrl }/dist/img/screenshot/block/evaluation-star.png`,
-	},
 	attributes,
 	edit,
 	save,
 	deprecated,
+	example,
 };
