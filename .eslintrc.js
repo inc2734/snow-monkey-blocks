5d2785a4e4fbc2fb4e9ee40433880c5cf76c543c
Takashi Kitajima <inc@2inc.org>
2019-12-14 14:40:37 +0900

    fix #243

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,9 @@
-module.exports = {
-	root: true,
-	extends: [
-		'plugin:@wordpress/eslint-plugin/recommended',
+const defaultConfig = require("@wordpress/scripts/config/.eslintrc.js");
 
-		// Temporary workaround to use linting rules for both e2e and unit tests with all files
-		// until override files globbing logic is fixed when using with --config.
-		// @see https://github.com/eslint/eslint/issues/11558
-		'plugin:@wordpress/eslint-plugin/test-e2e',
-		'plugin:@wordpress/eslint-plugin/test-unit',
-	],
+module.exports = {
+	...defaultConfig,
 	globals: {
+		...defaultConfig.globals,
 		smb: true,
 	},
 };
