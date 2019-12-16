e59590c3571ba3fd4733aa655e99cb7c5008f1b8
Takashi Kitajima <inc@2inc.org>
2019-12-16 12:00:32 +0900

    #240 step

---
+++ block/step/block/example.js
@@ -0,0 +1,34 @@
+'use strict';
+
+export default {
+	innerBlocks: [
+		{
+			name: 'snow-monkey-blocks/step--item',
+			attributes: {
+				title: 'Lorem ipsum dolor sit amet',
+			},
+			innerBlocks: [
+				{
+					name: 'core/paragraph',
+					attributes: {
+						content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+					},
+				},
+			],
+		},
+		{
+			name: 'snow-monkey-blocks/step--item',
+			attributes: {
+				title: 'consectetur adipiscing elit',
+			},
+			innerBlocks: [
+				{
+					name: 'core/paragraph',
+					attributes: {
+						content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+					},
+				},
+			],
+		},
+	],
+};
--- block/step/block/index.js
+++ block/step/block/index.js
@@ -4,6 +4,7 @@ import blockConfig from '../../../src/js/config/block';
 import edit from './edit';
 import save from './save';
 import deprecated from './deprecated';
+import example from './example';
 
 import {
 	__,
@@ -19,10 +20,8 @@ export const settings = {
 		src: 'editor-ol',
 	},
 	category: blockConfig.blockCategories.common,
-	snowMonkeyBlocks: {
-		screenshot: `${ smb.pluginUrl }/dist/img/screenshot/block/step.png`,
-	},
 	edit,
 	save,
 	deprecated,
+	example,
 };
