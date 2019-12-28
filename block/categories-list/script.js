3af73af065eebfdbb73bb9b154067f4db9da500d
Takashi Kitajima <inc@2inc.org>
2019-12-28 23:30:28 +0900

    7.2.4

--- block/categories-list/script.js
+++ block/categories-list/script.js
@@ -4,9 +4,10 @@ import forEachHtmlNodes from '@inc2734/for-each-html-nodes';
 import CategoriesList from './categories-list';
 
 const applyCategoriesList = () => {
-	const htmlNodes = document.querySelectorAll( '.smb-categories-list__list' );
-	const nodes = Array.prototype.slice.call(htmlNodes, 0);
-	nodes.forEach((htmlNode) => new CategoriesList( htmlNode ));
+	forEachHtmlNodes(
+		document.querySelectorAll( '.smb-categories-list__list' ),
+		( categoryList ) => new CategoriesList( categoryList )
+	);
 };
 
 document.addEventListener(
