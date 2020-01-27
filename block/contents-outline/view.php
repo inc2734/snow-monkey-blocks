e2cc405f7a3874457164683ea591b8d8af79904f
Takashi Kitajima <inc@2inc.org>
2020-01-27 14:57:57 +0900

    fix #258

--- block/contents-outline/view.php
+++ block/contents-outline/view.php
@@ -11,7 +11,7 @@ if ( ! shortcode_exists( 'wp_contents_outline' ) ) {
 
 echo do_shortcode(
 	sprintf(
-		'[wp_contents_outline post_id="%1$d" selector=".c-entry__content" headings="%2$s" move_to_before_1st_heading="%3$s" id="%4$s" class="%5$s"]',
+		'[wp_contents_outline post_id="%1$d" selector=".c-entry__content, .c-entry__content .wp-block-group__inner-container" headings="%2$s" move_to_before_1st_heading="%3$s" id="%4$s" class="%5$s"]',
 		get_the_ID(),
 		$attributes['headings'],
 		$attributes['moveToBefore1stHeading'] ? 'true' : 'false',
