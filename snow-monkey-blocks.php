bd85eb8b2f74f7f5638b8448ee6d49347b790f0d
Takashi Kitajima <inc@2inc.org>
2020-02-05 13:54:25 +0900

    7.3.3

--- snow-monkey-blocks.php
+++ snow-monkey-blocks.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin name: Snow Monkey Blocks
- * Version: 7.3.2
+ * Version: 7.3.3
  * Description: Gutenberg blocks collection made by MonkeyWrench.
  * Author: inc2734
  * Author URI: https://2inc.org
