da6d21a01c3fb5581bbab68e6a1caa2e1161999d
Takashi Kitajima <inc@2inc.org>
2020-07-03 10:20:03 +0900

    fix #316

--- block-template/heading-2/template.php
+++ block-template/heading-2/template.php
@@ -10,7 +10,7 @@
 <div class="wp-block-snow-monkey-blocks-section-with-bgimage alignfull smb-section smb-section-with-bgimage smb-section-with-bgimage--left smb-section-with-bgimage--wide" style="color:#fff">
 	<div class="smb-section-with-bgimage__bgimage smb-section-with-bgimage__bgimage--lg">
 		<div class="smb-section-with-bgimage__mask"></div>
-		<img src="http://snow-monkey.local/wp-content/plugins/snow-monkey-blocks/dist/img/photos/mountain-landscape-and-tree-at-sunset.jpg" alt="" class="wp-image-0" style="opacity:0.6"/>
+		<img src="<?php echo esc_url( SNOW_MONKEY_BLOCKS_DIR_URL ); ?>/dist/img/photos/mountain-landscape-and-tree-at-sunset.jpg" alt="" class="wp-image-0" style="opacity:0.6"/>
 	</div>
 	<div class="c-container">
 		<div class="smb-section__body">
@@ -35,7 +35,7 @@
 					</div>
 					<div class="c-row__col c-row__col--1-1 c-row__col--lg-1-2">
 						<div class="smb-media-text__figure">
-							<img src="http://snow-monkey.local/wp-content/plugins/snow-monkey-blocks/dist/img/photos/man-sitting-at-table-with-coffee-cup-using-digital-tablet.jpg" alt="" class="wp-image-0"/>
+							<img src="<?php echo esc_url( SNOW_MONKEY_BLOCKS_DIR_URL ); ?>/dist/img/photos/man-sitting-at-table-with-coffee-cup-using-digital-tablet.jpg" alt="" class="wp-image-0"/>
 						</div>
 					</div>
 				</div>
