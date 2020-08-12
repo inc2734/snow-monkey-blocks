43e1a459d0434b30a3fee9a9534c1ed5d9bc5b0b
Takashi Kitajima <inc@2inc.org>
2020-08-12 10:59:50 +0900

    9.0.1

---
+++ block/step/item/standard/index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package snow-monkey-blocks
+ * @author inc2734
+ * @license GPL-2.0+
+ */
+
+use Snow_Monkey\Plugin\Blocks;
+
+/**
+ * editor_script
+ */
+$asset = include( SNOW_MONKEY_BLOCKS_DIR_PATH . '/dist/block/step/item/standard/editor.asset.php' );
+wp_register_script(
+	'snow-monkey-blocks/step/item/standard/editor',
+	SNOW_MONKEY_BLOCKS_DIR_URL . '/dist/block/step/item/standard/editor.js',
+	array_merge( $asset['dependencies'], [ 'snow-monkey-blocks-editor' ] ),
+	filemtime( SNOW_MONKEY_BLOCKS_DIR_PATH . '/dist/block/step/item/standard/editor.js' ),
+	true
+);
+
+register_block_type_from_metadata(
+	__DIR__,
+	[
+		'editor_script' => 'snow-monkey-blocks/step/item/standard/editor',
+	]
+);
