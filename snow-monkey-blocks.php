0b0427fef1b3f5ded458639582a00ae815284d5c
inc2734 <inc@2inc.org>
2021-07-28 10:11:00 +0900

    12.0.0-beta3

--- snow-monkey-blocks.php
+++ snow-monkey-blocks.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin name: Snow Monkey Blocks
- * Version: 12.0.0-beta2
+ * Version: 12.0.0-beta3
  * Description: Gutenberg blocks collection made by MonkeyWrench.
  * Author: inc2734
  * Author URI: https://2inc.org
