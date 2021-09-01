cb538cd5ad32f106a40c1eb751abfbaf81d90406
inc2734 <inc@2inc.org>
2021-09-01 15:25:32 +0900

    Fix #496

--- patterns/text-with-image-2/pattern.php
+++ patterns/text-with-image-2/pattern.php
@@ -8,21 +8,23 @@
 
 <!-- wp:snow-monkey-blocks/section-break-the-grid {"textColor":"#fff","imageSize":"xl","contentHorizontalPosition":"xs","contentBackgroundColor":"#000","contentPadding":"s","align":"full"} -->
 <div class="wp-block-snow-monkey-blocks-section-break-the-grid alignfull smb-section smb-section-break-the-grid smb-section-break-the-grid--right" style="color:#fff">
-	<div class="c-container">
-		<div class="c-row c-row--margin c-row--lg-middle">
-			<div class="c-row__col c-row__col--1-1 c-row__col--lg-1-2">
-				<div class="smb-section-break-the-grid__content smb-section-break-the-grid__content--w-s smb-section-break-the-grid__content--p-s smb-section-break-the-grid__content--horizontal-xs" style="background-color:rgba(0, 0, 0, 0.98)">
-					<h2 class="smb-section__title smb-section-break-the-grid__title">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h2>
-					<div class="smb-section__body smb-section-break-the-grid__body">
-						<!-- wp:paragraph -->
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillu</p>
-						<!-- /wp:paragraph -->
+	<div class="smb-section__inner">
+		<div class="c-container">
+			<div class="c-row c-row--margin c-row--lg-middle">
+				<div class="c-row__col c-row__col--1-1 c-row__col--lg-1-2">
+					<div class="smb-section-break-the-grid__content smb-section-break-the-grid__content--w-s smb-section-break-the-grid__content--p-s smb-section-break-the-grid__content--horizontal-xs" style="background-color:rgba(0, 0, 0, 0.98)">
+						<h2 class="smb-section__title smb-section-break-the-grid__title">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h2>
+						<div class="smb-section__body smb-section-break-the-grid__body">
+							<!-- wp:paragraph -->
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillu</p>
+							<!-- /wp:paragraph -->
+						</div>
 					</div>
 				</div>
-			</div>
-			<div class="c-row__col c-row__col--1-1 c-row__col--lg-1-2">
-				<div class="smb-section-break-the-grid__figure smb-section-break-the-grid__figure--w-xl">
-					<img src="<?php echo esc_url( SNOW_MONKEY_BLOCKS_DIR_URL ); ?>/dist/img/photos/0117_xlarge.jpg" alt="" class="wp-image-0"/>
+				<div class="c-row__col c-row__col--1-1 c-row__col--lg-1-2">
+					<div class="smb-section-break-the-grid__figure smb-section-break-the-grid__figure--w-xl">
+						<img src="<?php echo esc_url( SNOW_MONKEY_BLOCKS_DIR_URL ); ?>/dist/img/photos/0117_xlarge.jpg" alt="" class="wp-image-0"/>
+					</div>
 				</div>
 			</div>
 		</div>
