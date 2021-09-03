f7c7f7e7b52456b9d4dfc413e65014dec96a3c9c
inc2734 <inc@2inc.org>
2021-09-03 19:40:27 +0900

    Update icons

--- block/limited-datetime/icon.js
+++ block/limited-datetime/icon.js
@@ -1,3 +1,13 @@
-export default(
-	<svg viewBox="0 0 24 24"><path d="M3,4V20a1,1,0,0,0,1,1H20a1,1,0,0,0,1-1V4a1,1,0,0,0-1-1H4A1,1,0,0,0,3,4ZM19.5,20H4.5a.5.5,0,0,1-.5-.5V7.5A.5.5,0,0,1,4.5,7h15a.5.5,0,0,1,.5.5v12A.5.5,0,0,1,19.5,20Z"/><rect x="5.5" y="10.5" width="2" height="2" rx="0.5"/><rect x="9.17" y="10.5" width="2" height="2" rx="0.5"/><rect x="12.83" y="10.5" width="2" height="2" rx="0.5"/><rect x="16.5" y="10.5" width="2" height="2" rx="0.5"/><rect x="5.5" y="14.5" width="2" height="2" rx="0.5"/><rect x="9.17" y="14.5" width="2" height="2" rx="0.5"/><rect x="12.83" y="14.5" width="2" height="2" rx="0.5"/><rect x="16.5" y="14.5" width="2" height="2" rx="0.5"/></svg>
+export default (
+	<svg viewBox="0 0 24 24">
+		<path d="M3,4V20a1,1,0,0,0,1,1H20a1,1,0,0,0,1-1V4a1,1,0,0,0-1-1H4A1,1,0,0,0,3,4ZM19.5,20H4.5a.5.5,0,0,1-.5-.5V7.5A.5.5,0,0,1,4.5,7h15a.5.5,0,0,1,.5.5v12A.5.5,0,0,1,19.5,20Z" />
+		<rect x="5.5" y="10.5" width="2" height="2" rx="0.5" />
+		<rect x="9.17" y="10.5" width="2" height="2" rx="0.5" />
+		<rect x="12.83" y="10.5" width="2" height="2" rx="0.5" />
+		<rect x="16.5" y="10.5" width="2" height="2" rx="0.5" />
+		<rect x="5.5" y="14.5" width="2" height="2" rx="0.5" />
+		<rect x="9.17" y="14.5" width="2" height="2" rx="0.5" />
+		<rect x="12.83" y="14.5" width="2" height="2" rx="0.5" />
+		<rect x="16.5" y="14.5" width="2" height="2" rx="0.5" />
+	</svg>
 );
--- block/panels/icon.js
+++ block/panels/icon.js
@@ -1,3 +1,8 @@
-export default(
-	<svg viewBox="0 0 24 24"><rect x="3" y="2.97" width="8.22" height="8.22"/><rect x="12.78" y="2.97" width="8.22" height="8.22"/><rect x="3" y="13.07" width="8.22" height="8.22"/><rect x="12.76" y="13.07" width="8.22" height="8.22"/></svg>
+export default (
+	<svg viewBox="0 0 24 24">
+		<rect x="3" y="2.97" width="8.22" height="8.22" />
+		<rect x="12.78" y="2.97" width="8.22" height="8.22" />
+		<rect x="3" y="13.07" width="8.22" height="8.22" />
+		<rect x="12.76" y="13.07" width="8.22" height="8.22" />
+	</svg>
 );
