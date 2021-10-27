3a9e5f1a3bd98e6921b8fb9eae92498c2293eb8a
inc2734 <inc@2inc.org>
2021-10-27 11:46:03 +0900

    Update blcok patterns #519

--- patterns/text-with-image-1/pattern.php
+++ patterns/text-with-image-1/pattern.php
@@ -6,13 +6,13 @@
  */
 ?>
 
-<!-- wp:snow-monkey-blocks/section-break-the-grid {"shadowColor":"#f7f7f7","align":"full"} -->
-<div class="wp-block-snow-monkey-blocks-section-break-the-grid alignfull smb-section smb-section-break-the-grid smb-section-break-the-grid--right">
+<!-- wp:snow-monkey-blocks/section-break-the-grid {"mobileOrder":"nowrap","shadowColor":"#f7f7f7","align":"full"} -->
+<div class="wp-block-snow-monkey-blocks-section-break-the-grid alignfull smb-section smb-section-break-the-grid smb-section-break-the-grid--right smb-section-break-the-grid--mobile-nowrap">
 	<div class="smb-section__inner">
 		<div class="c-container">
 			<div class="smb-section__contents-wrapper smb-section-break-the-grid__contents-wrapper">
 				<div class="c-row c-row--lg-middle">
-					<div class="c-row__col c-row__col--1-1 c-row__col--lg-1-2">
+					<div class="c-row__col c-row__col--1-2">
 						<div class="smb-section-break-the-grid__content smb-section-break-the-grid__content--w--20 smb-section-break-the-grid__content--horizontal-10">
 							<div class="smb-section__header smb-section-break-the-grid__header">
 								<h2 class="smb-section__title smb-section-break-the-grid__title">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h2>
@@ -24,7 +24,7 @@
 							</div>
 						</div>
 					</div>
-					<div class="c-row__col c-row__col--1-1 c-row__col--lg-1-2">
+					<div class="c-row__col c-row__col--1-2">
 						<div class="smb-section-break-the-grid__figure smb-section-break-the-grid__figure--w-40">
 							<div class="smb-section-break-the-grid__shadow" style="background-color:#f7f7f7;transform:translate(5%, 5%)"></div>
 							<img src="<?php echo esc_url( SNOW_MONKEY_BLOCKS_DIR_URL ); ?>/dist/img/photos/0135_xlarge.jpg" alt="" class="wp-image-0"/>
--- patterns/text-with-image-2/pattern.php
+++ patterns/text-with-image-2/pattern.php
@@ -6,13 +6,13 @@
  */
 ?>
 
-<!-- wp:snow-monkey-blocks/section-break-the-grid {"textColor":"#fff","imageSize":"80","contentHorizontalPosition":"5","contentBackgroundColor":"#000","contentPadding":"s","align":"full"} -->
-<div class="wp-block-snow-monkey-blocks-section-break-the-grid alignfull smb-section smb-section-break-the-grid smb-section-break-the-grid--right" style="color:#fff">
+<!-- wp:snow-monkey-blocks/section-break-the-grid {"mobileOrder":"nowrap","textColor":"#fff","imageSize":"80","contentHorizontalPosition":"5","contentBackgroundColor":"#000","contentPadding":"s","align":"full"} -->
+<div class="wp-block-snow-monkey-blocks-section-break-the-grid alignfull smb-section smb-section-break-the-grid smb-section-break-the-grid--right smb-section-break-the-grid--mobile-nowrap" style="color:#fff">
 	<div class="smb-section__inner">
 		<div class="c-container">
 			<div class="smb-section__contents-wrapper smb-section-break-the-grid__contents-wrapper">
 				<div class="c-row c-row--lg-middle">
-					<div class="c-row__col c-row__col--1-1 c-row__col--lg-1-2">
+					<div class="c-row__col c-row__col--1-2">
 						<div class="smb-section-break-the-grid__content smb-section-break-the-grid__content--w--20 smb-section-break-the-grid__content--p-s smb-section-break-the-grid__content--horizontal-5" style="background-color:rgba(0, 0, 0, 0.98)">
 							<div class="smb-section__header smb-section-break-the-grid__header">
 								<h2 class="smb-section__title smb-section-break-the-grid__title">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h2>
@@ -24,7 +24,7 @@
 							</div>
 						</div>
 					</div>
-					<div class="c-row__col c-row__col--1-1 c-row__col--lg-1-2">
+					<div class="c-row__col c-row__col--1-2">
 						<div class="smb-section-break-the-grid__figure smb-section-break-the-grid__figure--w-80">
 							<img src="<?php echo esc_url( SNOW_MONKEY_BLOCKS_DIR_URL ); ?>/dist/img/photos/0117_xlarge.jpg" alt="" class="wp-image-0"/>
 						</div>
