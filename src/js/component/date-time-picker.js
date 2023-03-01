202eefc4cba2ce7deaf4411508d15cb01b0a5af3
inc2734 <inc@2inc.org>
2023-03-01 11:27:55 +0900

    #651 wp.date.__experimentalGetSettings

--- src/js/component/date-time-picker.js
+++ src/js/component/date-time-picker.js
@@ -1,12 +1,10 @@
-'use select';
-
 import { Button, DateTimePicker } from '@wordpress/components';
-import { __experimentalGetSettings } from '@wordpress/date';
+import { getSettings } from '@wordpress/date';
 import { __ } from '@wordpress/i18n';
 
 export default function ( { currentDate, onChange, onReset } ) {
 	// @see https://developer.wordpress.org/block-editor/components/date-time/#usage
-	const settings = __experimentalGetSettings();
+	const settings = getSettings();
 	const is12HourTime = /a(?!\\)/i.test(
 		settings.formats.time
 			.toLowerCase()
