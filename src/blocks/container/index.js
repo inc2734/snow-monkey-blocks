1f724c38757d88b51f0f53e7c3a6ebaf645a8d7e
inc2734 <inc@2inc.org>
2023-06-12 23:41:17 +0900

    Fix #723

--- src/blocks/container/index.js
+++ src/blocks/container/index.js
@@ -6,6 +6,7 @@ import metadata from './block.json';
 import icon from './icon';
 import edit from './edit';
 import save from './save';
+import transforms from './transforms';
 
 registerBlockType( metadata.name, {
 	icon: {
@@ -14,4 +15,5 @@ registerBlockType( metadata.name, {
 	},
 	edit,
 	save,
+	transforms,
 } );
