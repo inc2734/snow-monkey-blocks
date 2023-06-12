5be87ccee69d68f250ed8c17ff0650546f392350
inc2734 <inc@2inc.org>
2023-06-12 23:41:00 +0900

    Fix #724

--- src/blocks/box/index.js
+++ src/blocks/box/index.js
@@ -6,6 +6,7 @@ import metadata from './block.json';
 import icon from './icon';
 import edit from './edit';
 import save from './save';
+import transforms from './transforms';
 import deprecated from './deprecated';
 
 registerBlockType( metadata.name, {
@@ -15,5 +16,6 @@ registerBlockType( metadata.name, {
 	},
 	edit,
 	save,
+	transforms,
 	deprecated,
 } );
