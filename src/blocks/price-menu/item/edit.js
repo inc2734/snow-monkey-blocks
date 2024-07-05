a54f953691e5c4d537ea17c6ee01d7f585a792d0
inc2734 <inc@2inc.org>
2024-07-05 23:43:00 +0900

    fix #811

--- src/blocks/price-menu/item/edit.js
+++ src/blocks/price-menu/item/edit.js
@@ -1,16 +1,61 @@
 import classnames from 'classnames';
 
-import { RichText, useBlockProps } from '@wordpress/block-editor';
+import {
+	FontSizePicker,
+	InspectorControls,
+	RichText,
+	useBlockProps,
+	getFontSizeClass,
+	getFontSizeObjectByValue,
+	useSettings,
+} from '@wordpress/block-editor';
+
+import {
+	BaseControl,
+	__experimentalToolsPanelItem as ToolsPanelItem,
+} from '@wordpress/components';
+
 import { __ } from '@wordpress/i18n';
 
-export default function ( { attributes, setAttributes, className } ) {
-	const { title, price } = attributes;
+import metadata from './block.json';
+
+export default function ( { attributes, setAttributes, className, clientId } ) {
+	const {
+		title,
+		price,
+		titleFontSizeSlug,
+		titleFontSize,
+		priceFontSizeSlug,
+		priceFontSize,
+	} = attributes;
+
+	const [ fontSizes ] = useSettings( 'typography.fontSizes' );
 
 	const classes = classnames( 'smb-price-menu__item', className );
 
 	const blockProps = useBlockProps( {
 		className: classes,
 	} );
+
+	const selectedTitleFontSize =
+		fontSizes.find(
+			( fontSize ) =>
+				!! titleFontSizeSlug && fontSize.slug === titleFontSizeSlug
+		)?.size || titleFontSize;
+
+	const selectedPriceFontSize =
+		fontSizes.find(
+			( fontSize ) =>
+				!! priceFontSizeSlug && fontSize.slug === priceFontSizeSlug
+		)?.size || priceFontSize;
+
+	const titleFontSizeClass = !! titleFontSizeSlug
+		? getFontSizeClass( titleFontSizeSlug )
+		: undefined;
+
+	const priceFontSizeClass = !! priceFontSizeSlug
+		? getFontSizeClass( priceFontSizeSlug )
+		: undefined;
 
 	const onChangeTitle = ( value ) =>
 		setAttributes( {
@@ -23,20 +68,126 @@ export default function ( { attributes, setAttributes, className } ) {
 		} );
 
 	return (
-		<div { ...blockProps }>
-			<RichText
-				className="smb-price-menu__item__title"
-				placeholder={ __( 'Write title…', 'snow-monkey-blocks' ) }
-				value={ title }
-				onChange={ onChangeTitle }
-			/>
+		<>
+			<InspectorControls group="typography">
+				<ToolsPanelItem
+					panelId={ clientId }
+					hasValue={ () =>
+						titleFontSizeSlug !== metadata.titleFontSizeSlug ||
+						titleFontSize !== metadata.titleFontSize
+					}
+					isShownByDefault
+					label={ __( 'Title font size', 'snow-monkey-blocks' ) }
+					resetAllFilter={ () => ( {
+						titleFontSizeSlug: metadata.titleFontSizeSlug,
+						titleFontSize: metadata.titleFontSize,
+					} ) }
+					onDeselect={ () => {
+						setAttributes( {
+							titleFontSizeSlug: metadata.titleFontSizeSlug,
+							titleFontSize: metadata.titleFontSize,
+						} );
+					} }
+				>
+					<BaseControl
+						label={ __( 'Title', 'snow-monkey-blocks' ) }
+						id="snow-monkey-blocks/price-menu-item/title-font-size"
+					>
+						<FontSizePicker
+							value={ selectedTitleFontSize }
+							onChange={ ( value ) => {
+								const fontSizeSlug = getFontSizeObjectByValue(
+									fontSizes,
+									value
+								).slug;
 
-			<RichText
-				className="smb-price-menu__item__price"
-				placeholder={ __( 'Write price…', 'snow-monkey-blocks' ) }
-				value={ price }
-				onChange={ onChangePrice }
-			/>
-		</div>
+								setAttributes( {
+									titleFontSizeSlug:
+										fontSizeSlug || undefined,
+									titleFontSize: ! fontSizeSlug
+										? value
+										: undefined,
+								} );
+							} }
+							withReset={ false }
+							withSlider
+						/>
+					</BaseControl>
+				</ToolsPanelItem>
+
+				<ToolsPanelItem
+					panelId={ clientId }
+					hasValue={ () =>
+						priceFontSizeSlug !== metadata.priceFontSizeSlug ||
+						priceFontSize !== metadata.priceFontSize
+					}
+					isShownByDefault
+					label={ __( 'Price font size', 'snow-monkey-blocks' ) }
+					resetAllFilter={ () => ( {
+						priceFontSizeSlug: metadata.priceFontSizeSlug,
+						priceFontSize: metadata.priceFontSize,
+					} ) }
+					onDeselect={ () => {
+						setAttributes( {
+							priceFontSizeSlug: metadata.priceFontSizeSlug,
+							priceFontSize: metadata.priceFontSize,
+						} );
+					} }
+				>
+					<BaseControl
+						label={ __( 'Price', 'snow-monkey-blocks' ) }
+						id="snow-monkey-blocks/price-menu-item/price-font-size"
+					>
+						<FontSizePicker
+							value={ selectedPriceFontSize }
+							onChange={ ( value ) => {
+								const fontSizeSlug = getFontSizeObjectByValue(
+									fontSizes,
+									value
+								).slug;
+
+								setAttributes( {
+									priceFontSizeSlug:
+										fontSizeSlug || undefined,
+									priceFontSize: ! fontSizeSlug
+										? value
+										: undefined,
+								} );
+							} }
+							withReset={ false }
+							withSlider
+						/>
+					</BaseControl>
+				</ToolsPanelItem>
+			</InspectorControls>
+
+			<div { ...blockProps }>
+				<RichText
+					className={ classnames(
+						'smb-price-menu__item__title',
+						titleFontSizeClass
+					) }
+					placeholder={ __( 'Write title…', 'snow-monkey-blocks' ) }
+					value={ title }
+					onChange={ onChangeTitle }
+					style={ {
+						fontSize: titleFontSize || undefined,
+					} }
+				/>
+
+				<RichText
+					className={ classnames(
+						'smb-price-menu__item__price',
+						priceFontSizeClass
+					) }
+					placeholder={ __( 'Write price…', 'snow-monkey-blocks' ) }
+					value={ price }
+					onChange={ onChangePrice }
+					style={ {
+						fontSize: priceFontSize || undefined,
+					} }
+				/>
+			</div>
+		</>
 	);
 }
--- src/blocks/price-menu/item/save.js
+++ src/blocks/price-menu/item/save.js
@@ -1,19 +1,54 @@
 import classnames from 'classnames';
 
-import { RichText, useBlockProps } from '@wordpress/block-editor';
+import {
+	RichText,
+	useBlockProps,
+	getFontSizeClass,
+} from '@wordpress/block-editor';
 
 export default function ( { attributes, className } ) {
-	const { title, price } = attributes;
+	const {
+		title,
+		price,
+		titleFontSizeSlug,
+		titleFontSize,
+		priceFontSizeSlug,
+		priceFontSize,
+	} = attributes;
 
 	const classes = classnames( 'smb-price-menu__item', className );
 
+	const titleFontSizeClass = !! titleFontSizeSlug
+		? getFontSizeClass( titleFontSizeSlug )
+		: undefined;
+
+	const priceFontSizeClass = !! priceFontSizeSlug
+		? getFontSizeClass( priceFontSizeSlug )
+		: undefined;
+
 	return (
 		<div { ...useBlockProps.save( { className: classes } ) }>
-			<div className="smb-price-menu__item__title">
+			<div
+				className={ classnames(
+					'smb-price-menu__item__title',
+					titleFontSizeClass
+				) }
+				style={ {
+					fontSize: titleFontSize || undefined,
+				} }
+			>
 				<RichText.Content value={ title } />
 			</div>
 
-			<div className="smb-price-menu__item__price">
+			<div
+				className={ classnames(
+					'smb-price-menu__item__price',
+					priceFontSizeClass
+				) }
+				style={ {
+					fontSize: priceFontSize || undefined,
+				} }
+			>
 				<RichText.Content value={ price } />
 			</div>
 		</div>
