d6423c080bbc5dc5ecdbb4febcc555e6d352a9ae
inc2734 <inc@2inc.org>
2025-01-30 10:35:57 +0900

    fix #842

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 const path = require( 'path' );
 
-const plugins = [ ...defaultConfig.plugins ];
+const plugins = [ ...defaultConfig?.plugins ];
 
 plugins.splice(1, 1); //delete plugins.CleanWebpackPlugin
-plugins.splice(2, 1); //delete plugins.CopyWebpackPlugin
+// plugins.splice(2, 1); //delete plugins.CopyWebpackPlugin
 
 module.exports = {
 	...defaultConfig,
