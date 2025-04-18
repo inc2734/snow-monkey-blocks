cf10b8ec3f3ae7eb037257cc560c2b40400eb0c0
inc2734 <inc@2inc.org>
2025-04-18 23:15:56 +0900

    fix #857

--- src/blocks/list/index.js
+++ src/blocks/list/index.js
@@ -1,6 +1,7 @@
 import blockConfig from '@smb/config/block';
 
 import { registerBlockType } from '@wordpress/blocks';
+import { addFilter } from '@wordpress/hooks';
 
 import metadata from './block.json';
 import icon from './icon';
@@ -20,3 +21,29 @@ registerBlockType( metadata.name, {
 	save,
 	deprecated,
 } );
+
+function addCoreListItemToIconList( settings, name ) {
+	if ( name !== 'core/list-item' ) {
+		return settings;
+	}
+
+	let parents = [];
+	if ( null != settings?.parent ) {
+		if ( Array.isArray( settings.parent ) ) {
+			parents = settings.parent;
+		} else {
+			parents = [ settings.parent ];
+		}
+	}
+
+	return {
+		...settings,
+		parent: [ ...parents, metadata.name ],
+	};
+}
+
+addFilter(
+	'blocks.registerBlockType',
+	'snow-monkey-blocks/add-core-list-item-to-icon-list',
+	addCoreListItemToIconList
+);
