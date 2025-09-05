46d4c7aa1469ee5aedf04dcf0865cb251466a81a
inc2734 <inc@2inc.org>
2025-09-05 11:40:16 +0900

    fix #867

--- src/blocks/pricing-table/item/index.js
+++ src/blocks/pricing-table/item/index.js
@@ -1,6 +1,7 @@
 import blockConfig from '@smb/config/block';
 
 import { registerBlockType } from '@wordpress/blocks';
+import { addFilter } from '@wordpress/hooks';
 
 import metadata from './block.json';
 import icon from '../icon';
@@ -17,3 +18,29 @@ registerBlockType( metadata.name, {
 	save,
 	deprecated,
 } );
+
+function addCoreListItemToPricingTable( settings, name ) {
+	if ( name !== 'core/list-item' ) {
+		return settings;
+	}
+
+	let parents = [];
+	if ( null != settings?.parent ) {
+		if ( Array.isArray( settings.parent ) ) {
+			parents = settings.parent;
+		} else {
+			parents = [ settings.parent ];
+		}
+	}
+
+	return {
+		...settings,
+		parent: [ ...parents, metadata.name ],
+	};
+}
+
+addFilter(
+	'blocks.registerBlockType',
+	'snow-monkey-blocks/add-core-list-item-to-pricing-table',
+	addCoreListItemToPricingTable
+);
