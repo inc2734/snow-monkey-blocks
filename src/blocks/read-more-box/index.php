8c3b50948117956e4a7af3dbee81dd76a3d8ffeb
inc2734 <inc@2inc.org>
2026-05-29 09:34:38 +0900

    fix #880

--- src/blocks/read-more-box/index.php
+++ src/blocks/read-more-box/index.php
@@ -9,6 +9,17 @@ register_block_type(
 	__DIR__
 );
 
+wp_enqueue_block_style(
+	'snow-monkey-blocks/read-more-box',
+	array(
+		'handle' => generate_block_asset_handle( 'snow-monkey-blocks/read-more-box', 'style' ),
+		'src'    => SNOW_MONKEY_BLOCKS_DIR_URL . '/dist/blocks/read-more-box/style-index.css',
+		'deps'   => array( generate_block_asset_handle( 'snow-monkey-blocks/btn', 'style' ) ),
+		'ver'    => filemtime( __DIR__ . '/style-index.css' ),
+		'file'   => __DIR__ . '/style-index.css',
+	)
+);
+
 /**
  * excerpt_allowed_blocks
  */
